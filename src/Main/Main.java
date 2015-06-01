df4bdf09ffc2a18595403d155d3d87666288416a
Tiven123 <steven.alejandro@hotmail.com>
2015-06-01 12:33:59 -0600

    final

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Cliente.*;
+import interfaz.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		InterfazCliente cliente=new InterfazCliente();
+		
+		System.out.println("Hola");
+
+	}
+
+}
